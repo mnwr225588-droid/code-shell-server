fe48be16c9947e302f32508ec32a036c72599d9c
Hossam <mnwr225588@gmail.com>
2026-08-03 19:06:13 +0300

    Add fillable properties to EmailVerification model

--- app/Models/EmailVerification.php
+++ app/Models/EmailVerification.php
@@ -2,9 +2,21 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class EmailVerification extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'token',
+        'expires_at',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
